1b0349663265698d8fcd522c02c9301983f27f86
scasasbuenas <s.casasbuenas@uniandes.edu.co>
2024-10-23 09:01:30 -0500

    Login Manager

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, jsonify, request, render_template, redirect, url_for, flash, session
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user
 from pymongo import MongoClient
 import bcrypt
 import requests
@@ -15,10 +16,29 @@ client = MongoClient('mongodb+srv://scasasbuenas:EaFOacx8sknELd1Y@cluster0.3wjlj
 db = client['ofipensiones']
 users = db.users
 
+# Bloquea el acceso a las rutas que requieran login
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+
+class User(UserMixin):
+    def __init__(self, user_json):
+        self.id = str(user_json['_id'])
+        self.email = user_json['email']
+
+@login_manager.user_loader
+def load_user(user_id):
+    user = users.find_one({'_id': ObjectId(user_id)})
+    if not user:
+        return None
+    return User(user)
+
+
 # Pagina principal
 @app.route('/')
 def home():
     return render_template('home.html')
+
 
 # Registro de usuarios
 @app.route('/register', methods=['GET', 'POST'])
@@ -40,6 +60,9 @@ def register():
         return redirect(url_for('login'))
     return render_template('register.html')
 
+from bson.objectid import ObjectId
+
+
 # Autenticación de usuarios
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -49,21 +72,32 @@ def login():
         user = users.find_one({'email': email})
         
         if user and bcrypt.checkpw(password.encode('utf-8'), user['password']):
-            session['user_id'] = str(user['_id'])
+            user_obj = User(user)
+            login_user(user_obj)  # Flask-Login gestiona la sesión
+            
             secret_response = request.form['g-recaptcha-response']
+            verify_response = requests.post(url=f'{VERIFY_URL}?secret={SECRET_KEY}&response={secret_response}').json()
             
-            verify_respose = requests.post(url=f'{VERIFY_URL}?secret={SECRET_KEY}&response={secret_response}').json()
-            
-            return redirect(url_for('dashboard'))
-        else:
-            flash('Credenciales inválidas. Intente nuevamente.', 'error')  # Etiqueta el mensaje como 'error'
-            
-    return render_template('login.html', site_key = SITE_KEY)
+            if verify_response.get('success'):
+                return redirect(url_for('dashboard'))
+            else:
+                flash('Error de reCAPTCHA. Por favor, inténtelo de nuevo.')
+        
+        flash('Credenciales inválidas. Intente nuevamente.', 'error')
+    
+    return render_template('login.html', site_key=SITE_KEY)
+
 
 # Dashboard después del login
 @app.route('/dashboard')
+@login_required
 def dashboard():
     return 'Bienvenido al Dashboard'
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('home'))
